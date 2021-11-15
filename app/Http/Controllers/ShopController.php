92c9e20ef492b7cdc6d74f4ad313fd69666a6a88
anasusb11 <anasusb11@gmail.com>
2021-11-15 21:14:25 +0700

    read data product and add pagination

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('shop.index');
+        // $products = Product::all();
+        // pagination
+        $products = Product::paginate(6);
+        return view('shop.index', compact('products'));
     }
 
     public function show()
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -32,27 +32,25 @@
           <h2>Our Products</h2>
           <hr style="margin-bottom: 2em;">
           <div class="row list-product">
-            <div class="col-lg-4 item">
+            @foreach ($products as $product)
+                <div class="col-lg-4 item mb-5">
               <a href="/shop/detail">
-              <img src="{{asset('storage/images/product.jpg')}}" alt="nopic" height="180" width="180">
+              <img src="{{asset($product->image)}}" alt="nopic" height="180" width="180">
               </a>
-              <p class="product-name mt-3 font-weight-bold"><a href="">Nama Produk</a></p>
-              <p class="product-price">Rp20000</p>
-            </div>
-            <div class="col-lg-4 item">
-              <a href="/shop/detail">
-              <img src="{{asset('storage/images/product.jpg')}}" alt="nopic" height="180" width="180">
-              </a>
-              <p class="product-name mt-3"><a href="">Nama Produk</a></p>
-              <p class="product-price">Rp20000</p>
+              <p class="product-name mt-3 font-weight-bold"><a href="">{{ $product->name }}</a></p>
+              <p class="product-price">Rp {{ number_format($product->price) }}</p>
+                          </div>
+
+            @endforeach
+            
             </div>
           </div>
+          <!-- Pagination Link -->
+          {{$products->links()}}
         </div>
       </div>
     </div>
   </div>
-  <!-- Pagination Link -->
-  {{-- {{$items->links()}} --}}
 </div>
 @endsection
 
