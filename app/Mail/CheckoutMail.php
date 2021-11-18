d814f65224700c038da12ab5647ff999ad0497a5
anasusb11 <anasusb11@gmail.com>
2021-11-18 10:40:56 +0700

    setting mail driver mailgun

---
+++ app/Mail/CheckoutMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CheckoutMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $product;
+    public function __construct($product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Your Transaction is Success!')->view('template.mail');
+    }
+}
---
+++ resources/views/template/mail.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div class="container">
+        <h1>You Have Placed an Order :</h1>
+        <table>
+            <thead>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Subtotal</th>
+            </thead>
+
+            <tbody>
+
+            </tbody>
+        </table>
+        <h2>Total Order:  </h2>
+    </div>
+
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
